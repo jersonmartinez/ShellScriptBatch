05ee51e127d839388eec57e07db5c65e1d11e79d
Jerson Martinez <sidemasterdelfuturo@gmail.com>
2018-06-28 11:24:27 -0600

    Repairing the variables that a string contain and that are passed by parameters to a function

--- CreateNetwork.bat
+++ CreateNetwork.bat
@@ -1,11 +1,11 @@
 @echo off
-	title Create Network \ Side Master
+	title Create Network
 	
 		:Init (
-			call :getUserNetwork & cls
+			call :getUserNetwork && cls
 			echo Nombre de red: %network_name%
 			
-			call :getPassNetwork %network_name%
+			call :getPassNetwork "%network_name%"
 			echo Clave de red: %network_pass%
 
 			goto :Finished
@@ -14,8 +14,8 @@
 		:getUserNetwork (
 			set /p network_name=Nombre de red: 
 
-			if /i ["%network_name%"]=="[]" (
-				cls & call :getUserNetwork
+			if ["%network_name%"]==[""] (
+				cls && call :getUserNetwork
 			)
 
 			exit /b 0
@@ -25,14 +25,12 @@
 			echo Nombre de red: %~1
 			set /p network_pass=Clave de red: 
 			
-			if [%network_pass%]==[] (
-				cls & call :getPassNetwork %~1
+			if ["%network_pass%"]==[""] (
+				cls && call :getPassNetwork "%~1"
 			)
 
 			exit /b 0
 		)
-
-		:Arguments
 
 		:Finished
 	pause>nul
